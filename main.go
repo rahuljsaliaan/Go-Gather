ff988d755c9ba4cf12d083d092b6d9333d3e592e
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-06-09 19:13:36 +0530

    experiment: :alembic: Prototype server and GET function

--- main.go
+++ main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+
+	server.Run(":8000")
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Hello"})
+}
