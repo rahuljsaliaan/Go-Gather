beef81d8eb061b63cacf03e3595c5bf6c6cddb41
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-06-11 21:22:37 +0530

    fix(jwt): :bug: Ensure token claims are valid and extract user ID

--- pkg/utils/jwt.go
+++ pkg/utils/jwt.go
@@ -12,13 +12,13 @@ func GenerateToken(email string, userID int64) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"email":  email,
 		"userID": userID,
-		"exp":    time.Now().Add(time.Hour * config.Env.JwtSecretExpire).Unix(),
+		"exp":    time.Now().Add(time.Hour * time.Duration(config.Env.JwtExpHours)).Unix(),
 	})
 
 	return token.SignedString(config.Env.JwtSecret)
 }
 
-func ValidateToken(token string) error {
+func ValidateToken(token string) (int64, error) {
 	parsedToken, err := jwt.Parse(token, func(t *jwt.Token) (any, error) {
 		_, ok := t.Method.(*jwt.SigningMethodHMAC) // assert/check the signing method type
 
@@ -30,21 +30,21 @@ func ValidateToken(token string) error {
 	})
 
 	if err != nil {
-		return err
+		return 0, err
 	}
 
 	if !parsedToken.Valid {
-		return errors.New("invalid token")
+		return 0, errors.New("invalid token")
 	}
 
-	// claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
 
-	// if !ok {
-	// 	return errors.New("invalid token claims")
-	// }
+	if !ok {
+		return 0, errors.New("invalid token claims")
+	}
 
 	// email := claims["email"].(string)
-	// userID := claims["userId"].(int64)
+	userID := int64(claims["userId"].(float64))
 
-	return nil
+	return userID, nil
 }
