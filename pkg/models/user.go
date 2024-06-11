82d64bf29ebe2896bff16d8891116ed6b3342738
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-06-11 21:19:52 +0530

    fix(user): :bug: Update user ID by passing struct pointer, not copy

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -50,7 +50,7 @@ func (user User) Save() error {
 	return nil
 }
 
-func (user User) ValidateCredentials() error {
+func (user *User) ValidateCredentials() error {
 	query := "SELECT id,password FROM users WHERE email = ?"
 	row := db.DB.QueryRow(query, user.Email)
 
